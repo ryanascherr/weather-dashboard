0c0c8a76d3e8c372b8c29754137b6219d91fcd57
ryanascherr <ryanascherr@gmail.com>
2021-03-25 11:00:31 -0400

    Added fetch request for 5-day forecast. Began to add text for 5-day forecast.

--- script.js
+++ script.js
@@ -3,41 +3,69 @@ var apiKey = "48429ee96c9bd15088c309d848c40e29"
 var searchBtn = $("#search-btn");
 var searchValue;
 var currentWeatherData;
+var forecast;
+var queryURL;
 
 function getSearchValue(){
     searchValue = $("#search-value").val();
     console.log(searchValue);
     getCurrentWeather(searchValue);
+    getForecast(searchValue);
 }
 
 function getCurrentWeather(searchValue){
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +
+    weatherQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +
     searchValue +
     "&appid=" +
     apiKey +
     "&units=imperial";
     console.log(queryURL);
 
-    fetch(queryURL)
+    fetch(weatherQueryURL)
+        .then(function (response){
+            if (!response.ok){
+                alert("Please enter valid location name.");
+                throw response.json();
+            }
+
+            return response.json();
+        })
+        .then(function (data){
+            console.log(data);
+            displayCurrentWeather(data);
+        })
+}
+
+function getForecast(){
+    forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    searchValue +
+    "&appid=" +
+    apiKey +
+    "&units=imperial";
+    console.log(queryURL);
+
+    fetch(forecastQueryURL)
         .then(function (response){
             return response.json();
         })
         .then(function (data){
             console.log(data);
-            currentWeatherData = data;
-            displayCurrentWeather();
+            displayForecast(data);
         })
 }
 
-function displayCurrentWeather(){
-    var cityName = currentWeatherData.name;
-    var temp = currentWeatherData.main.temp;
-    var humidity = currentWeatherData.main.humidity;
-    var windSpeed = currentWeatherData.wind.speed;
-    $("#current-name").text(cityName);
-    $("#current-temp").text(temp);
-    $("#current-humidity").text(humidity);
-    $("#current-wind").text(windSpeed);
+function displayCurrentWeather(data){
+    $("#current-name").text(data.name);
+    $("#current-temp").text("Temperature: " + data.main.temp + " °F");
+    $("#current-humidity").text("Humidity: " + data.main.humidity + "%");
+    $("#current-wind").text("Wind Speed: " + data.wind.speed + " MPH");
+}
+
+function displayForecast(data){
+    $(".hidden").removeClass("hidden");
+    $(".test").children().eq(1).children().first().text(moment().add(1, 'days').format("l"));
+    $(".test").children().eq(1).children().eq(2).text("Temp: " + data.list[7].main.temp + " °F");
+    $(".test").children().eq(1).children().eq(3).text("Humidity: " + data.list[7].main.humidity + "%");
 }
 
 searchBtn.click(getSearchValue);
